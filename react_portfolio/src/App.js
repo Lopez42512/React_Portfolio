3347fd8f6e0f898fd7eba61bd3033903447a6f18
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-01-12 14:48:06 -0500

    Setting up folders and getting the page to switch from my information to my projects

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import Main from "./Main"
+import Work from "./Work"
+import Header from "./Header"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            number: 0
+        }
+    }
+
+    handlesubmit = (event) => {
+        event.preventDefault()
+        console.log("hello")
+        const eventTarget = event.target.name
+        console.log(eventTarget)
+        eventTarget === "Main" ? this.setState({number: 0}) : this.setState({number:1})
+    }
+
+    render(){
+        console.log(this.state.number)
+        const page = this.state.number === 0 ? <Main /> : <Work />
+
+        return(
+            <div>
+                <Header />
+                <button onClick={this.handlesubmit} name="Main"  >Main</button>
+                <button onClick={this.handlesubmit} name="Work" >Work</button>
+                {page}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
