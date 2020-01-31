0c58bfbfaed175cd85d1b0303763ef9d39ee3aa7
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-01-31 13:47:45 -0500

    email update

--- src/Contact.js
+++ src/Contact.js
@@ -1,35 +1,30 @@
-import React from "react"
+import React from "react";
 
 function Contact() {
-    return(
-        <div>
-            <div className="iContainer">
-                <h2 className="iHelp">
-                    I can Help
-                </h2>
-            </div>
-            <div className="iContainer">
-                <div className="iHelp1">
-                    I'm Currently available for full-time and freelance work. <br />
-                    So, if you have a team I would be fit for 
-                    or a project <br /> you want to get started on,
-                    let's get in touch.
-                </div>
-            </div>
-            <div className="iContainer1">
-                <div className="box">
-                    <h1 className="help">HEllo world</h1>
-                    <ul>
-                        <li>
-                            <a href="mailTo:Lopez42512@gmail.com?Subject=Hello%20Ismael">
-                                Email: Lopez42512@gmail.com  
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="iContainer">
+        <h2 className="iHelp">I can Help</h2>
+      </div>
+      <div className="iContainer1">
+        <div className="box">
+          <div className="iHelp1">
+            I'm Currently available for full-time and freelance work. <br />
+            So, if you have a team I would be fit for or a project <br /> you
+            want to get started on, let's get in touch.
+          </div>
+          <ul>
+            <li>
+              Email: Lopez42512@gmail.com
+              <a href="mailTo:Lopez42512@gmail.com?Subject=Hello%20Ismael">
+                <i class="fa fa-envelope" style={{ fontSize: "28px" }}></i>
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
